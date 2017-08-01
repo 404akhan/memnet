9c1b6f5166d5f16d4977cf0801c432ec24d9fa29
404akhan <404akhan@gmail.com>
2017-08-01 21:55:04 +0900

    def

--- model.py
+++ model.py
@@ -190,7 +190,8 @@ class RN(nn.Module):
         # objects | 25 x 64 x 37
 
         inv_idx = Variable(torch.arange(objects.size(0)-1, -1, -1).long())
-        inv_objects = objects[inv_idx]
+        inv_objects = objects.index_select(0, inv_idx)
+        # inv_objects = objects[inv_idx]
 
         outputs1 = self.mnet(objects)
         outputs2 = self.mnet(inv_objects)
